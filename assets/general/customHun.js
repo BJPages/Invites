a8c5493be361a0fd4f5d0b4cbd93aef77ead42af
BJPages <142616609+BJPages@users.noreply.github.com>
2026-04-16 18:55:24 -0600

    Update customHun.js

--- assets/general/customHun.js
+++ assets/general/customHun.js
@@ -1,2 +1,3 @@
-document.getElementsByClassName("title")[0].style.marginTop = "80px";
+document.getElementsByClassName("title")[0].style.marginTop = "100px";
 document.getElementsByClassName("title")[0].innerHTML="Sofi cumple 7 <br>y <br>Carmin cumple 2";
+document.getElementById("detailsSection").style.top = "520px";
